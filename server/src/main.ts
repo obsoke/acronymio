0b80da80f48c9397baf06dd64deab1e1b14587ca
Dale Karp <obsoke@obsoke.com>
2023-10-21 20:38:01 -0400

    wip

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ if (import.meta.main) {
 
 class Player {
   #socket: WebSocket;
+  #submission: string = '';
 
   constructor(socket: WebSocket) {
     this.#socket = socket;
@@ -13,6 +14,38 @@ class Player {
   sendMessage(message: string) {
     this.#socket.send(message);
   }
+
+  setSubmission(submission: string) {
+    this.#submission = submission;
+  }
+
+  getSubmission(): string {
+    return this.#submission;
+  }
+}
+
+/*
+ * What are the game states?
+ *
+ * waiting: waiting for N players before the game can begin. when all players are ready, we move to..
+ *
+ * acronym: generate acronym, send it to players and start a countdown. players then submit their entries.
+ * WHEN: all entries have been received OR timer expires, we move to the next state:
+ *
+ * judging: the submitted acronyms are sent to the players and they vote on which they like the best. another time begins
+ * WHEN: all judgements have been received OR timer expires, we move the final state:
+ *
+ * gameover: the winner is revealed, the game has concluded!
+ */
+type State = 'waiting' | 'acronym' | 'judging' | 'gameover';
+
+/**
+ * A `Round` is a single game of Acronymio. It holds all data requires to run the game,
+ * transitions between states and deals with messages from/to players.
+ */
+class Round {
+  #state: State = 'waiting';
+  // #players:
 }
 
 const room: Record<string, Player> = {};
@@ -49,12 +82,6 @@ function startWebSocketServer() {
   });
 }
 
-/*
- * What are the game states?
- *
- * waiting: waiting for N players before the game can begin
- * acronym: generate acronym, send it to players and start a countdown
- */
 function gameLoop() {
   console.log('State of room: ', room);
 }
